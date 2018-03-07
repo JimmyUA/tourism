d3fe4576988ed4dbd3dcf5fd4cfc023a29cc895d
Jimmy <jimmy@comp.ua.exirigo-upos.pl>
2018-03-07 13:53:09 +0200

    added values to tourist info page

--- src/main/java/application/web/pages/handletour/HandleTourPage.java
+++ src/main/java/application/web/pages/handletour/HandleTourPage.java
@@ -2,8 +2,12 @@ package application.web.pages.handletour;
 
 import application.service.TouristService;
 import application.web.pages.base.BasePage;
+import application.web.pages.touristinfo.TouristInfoPage;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 
@@ -33,6 +37,12 @@ public class HandleTourPage extends BasePage {
 		loginLink.setVisible(false);
 		logoutLink.setVisible(true);
 		cabinetLink.setVisible(true);
+	}
+
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		response.render(CssHeaderItem.forReference(new CssResourceReference(HandleTourPage.class, "style.css")));
 	}
 
 }
--- src/main/java/application/web/pages/touristinfo/TouristInfoPage.java
+++ src/main/java/application/web/pages/touristinfo/TouristInfoPage.java
@@ -3,17 +3,19 @@ package application.web.pages.touristinfo;
 import application.entity.Tourist;
 import application.service.TouristService;
 import application.web.pages.base.BasePage;
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import application.web.pages.home.HomePage;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -23,6 +25,7 @@ public class TouristInfoPage extends BasePage{
     protected Tourist tourist;
     private Form<Void> form;
     private List<Label> labels;
+    private List<Label> values;
 
     @SpringBean
     private TouristService touristService;
@@ -31,6 +34,21 @@ public class TouristInfoPage extends BasePage{
     protected void onInitialize() {
         super.onInitialize();
         setUpNavigationLinks();
+        addSearchForm();
+        setLabels();
+        setValueContainers();
+    }
+
+    private void addSearchForm() {
+        form = new Form<Void>("form");
+        form.setOutputMarkupId(true);
+
+        touristSearch = new TextField<>("touristSearch", Model.of(""));
+        Button search = getSearchButton();
+
+        add(form);
+        form.add(touristSearch);
+        form.add(search);
     }
 
     private void setUpNavigationLinks() {
@@ -38,17 +56,20 @@ public class TouristInfoPage extends BasePage{
         logoutLink.setVisible(true);
         cabinetLink.setVisible(true);
 
-        form = new Form<Void>("form");
-        form.setOutputMarkupId(true);
+    }
 
-        setLabels();
+    private void setValueContainers() {
+        Label loginValue = new Label("loginValue",Model.of());
+        Label mobileValue = new Label("mobileValue",Model.of());
+        Label emailValue = new Label("emailValue",Model.of());
+        Label bonusValue = new Label("bonusValue",Model.of());
+        Label usedBonusValue = new Label("usedBonusValue",Model.of());
+        Label parentLoginValue = new Label("parentLoginValue",Model.of());
+        Label infoValue = new Label("infoValue",Model.of());
 
-        touristSearch = new TextField<>("touristSearch", Model.of(""));
-        AjaxButton search = getSearchButton();
-
-        add(form);
-        form.add(touristSearch);
-        form.add(search);
+        values = Arrays.asList(loginValue, mobileValue, emailValue, bonusValue, usedBonusValue, parentLoginValue, infoValue);
+        values.forEach(value -> value.setVisible(false).setOutputMarkupId(true));
+        add(values.stream().toArray(Label[]::new));
     }
 
     private void setLabels() {
@@ -65,22 +86,41 @@ public class TouristInfoPage extends BasePage{
 
         labels.forEach(label -> label.setVisible(false).setOutputMarkupId(true));
 
-        add(login, mobile, email, bonus, usedBonus, parentLogin, info);
+        add(labels.stream().toArray(Label[]::new));
     }
 
-    private AjaxButton getSearchButton() {
-        return new AjaxButton("search") {
+    private Button getSearchButton() {
+        return new Button("search") {
             @Override
-            protected void onSubmit(AjaxRequestTarget target) {
+            public void onSubmit() {
                 String touristMobile = touristSearch.getDefaultModelObjectAsString();
-
                 tourist = touristService.getByMobile(touristMobile);
                 labels.forEach(label -> label.setVisible(true));
-                target.add(labels.stream().toArray(Label[]::new));
-
+                mapValues();
+                values.forEach(value -> value.setVisible(true));
             }
         };
     }
 
+    private void mapValues() {
+        values.get(0).setDefaultModel(Model.of(" " + tourist.getLogin()));
+        values.get(1).setDefaultModel(Model.of(" " + tourist.getMobile()));
+        values.get(2).setDefaultModel(Model.of(" " + tourist.getEmail()));
+        values.get(3).setDefaultModel(Model.of(" " + tourist.getBonusAmount()/100.0));
+        values.get(4).setDefaultModel(Model.of(" " + tourist.getUsedBonuses()/100.0));
+        Tourist parent = tourist.getParent();
+        if (parent == null){
+            parent = new Tourist();
+            parent.setLogin("Denis");
+        }
 
+        values.get(5).setDefaultModel(Model.of(" " + parent.getLogin()));
+        values.get(6).setDefaultModel(Model.of(" " + tourist.getTouristInfoId()));
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        response.render(CssHeaderItem.forReference(new CssResourceReference(TouristInfoPage.class, "style.css")));
+    }
 }
